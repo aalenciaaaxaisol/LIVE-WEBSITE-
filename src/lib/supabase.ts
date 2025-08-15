b011f5e45065b9ec27775be8ec724465aa4302c6
aalenciaaaxaisol <aalenciaindustries@gmail.com>
2025-08-15 18:27:24 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// Replace with your own values
-const supabaseUrl = 'https://affocifovpghakhqvjva.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmZm9jaWZvdnBnaGFraHF2anZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MTkyNDEsImV4cCI6MjA1Njk5NTI0MX0.n9YTaKNzSbPdl6Cs2fUyaRb8t5PS-Bs-9Pjuri7qJ_o'
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
